c7b115afa2a5da646f9b3d4836095b1c6af270c4
Erzhan Nurdavletov <nurdavletoverjan@gmail.com>
2024-10-07 21:21:52 +0600

    added for loop for replacing boxes

--- src/ArrayProject.java
+++ src/ArrayProject.java
@@ -13,59 +13,62 @@ public class ArrayProject {
         rightNumbers[1] = 2;
         rightNumbers[2] = 3;
 
+        //for (int i = 0; i < 3; i++) {
+        //    rightNumbers[i] = random.nextInt(7) + 1;
+        //}
 
 
         int[] inputNumbers = new int[3];
         int countRightAnswers = 0;
-        //boolean Alltrue = false;
-        int countAttempts = 5;
 
+        int numberAttempts = 5;
 
+        System.out.println("Enter three numbers(1, 2, 3, 4, 5, 6, 7)");
 
-
-        while (countAttempts > 0) {
-            for (int i = 0; i < 3; i ++) {
+        while (numberAttempts > 0) {
+            for (int i = 0; i < 3; i++) {
                 inputNumbers[i] = sc.nextInt();
                 if (inputNumbers[i] == rightNumbers[i]) {
-                    countRightAnswers ++;
+                    countRightAnswers++;
                 }
             }
-            countAttempts --;
+            numberAttempts--;
             if (countRightAnswers == 3) {
                 System.out.println("all boxes are found");
                 break;
-                //Alltrue = true;
+
             }
             if (countRightAnswers == 1) {
-                System.out.println("1 out of 3 is correct. " + countAttempts + " attempts left");
+                System.out.println("1 out of 3 is correct. " + numberAttempts + " attempts left");
             }
             if (countRightAnswers == 2) {
-                System.out.println("2 out of 3 are correct. " + countAttempts + " attempts left");
+                System.out.println("2 out of 3 are correct. " + numberAttempts + " attempts left");
             }
             if (countRightAnswers == 0) {
-                System.out.println("0 out of 3 are correct. " + countAttempts + " attempts left");
+                System.out.println("0 out of 3 are correct. " + numberAttempts + " attempts left");
             }
 
             countRightAnswers = 0;
 
-            if (countAttempts == 0) {
-                countAttempts = 5;
+            if (numberAttempts == 0) {
+                numberAttempts = 5;
                 System.out.println("5 attempts are over, boxes replaced");
                 for (int i = 0; i < 3; i++) {
                     rightNumbers[i] = random.nextInt(3) + 1;
 
                 }
+                while (rightNumbers[0] == rightNumbers[1] || rightNumbers[1] == rightNumbers[2] || rightNumbers[0] == rightNumbers[2]) {
+                    for (int i = 0; i < 3; i++) {
+                        rightNumbers[i] = random.nextInt(7) + 1;
+
+
+                    }
+
+                }
+
             }
 
+
         }
-
-
-
-
-
-
-
-
-
     }
 }
